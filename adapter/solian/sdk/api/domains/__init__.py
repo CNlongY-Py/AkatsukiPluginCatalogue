b243e94262eb88de0c0c83c043db0a393e36e768
CNlongY-Py <79128694+CNlongY-Py@users.noreply.github.com>
2026-06-22 20:10:04 +0800

    Add files via upload

---
+++ adapter/solian/sdk/api/domains/__init__.py
@@ -0,0 +1,39 @@
+from .accounts_api import AccountsApi
+from .auth_api import AuthApi
+from .chat_api import ChatApi
+from .developers_api import DevelopersApi
+from .drive_api import DriveApi
+from .e2ee_api import E2EEApi
+from .fitness_api import FitnessApi
+from .notifications_api import NotificationsApi
+from .padlock_api import PadlockApi
+from .payments_api import PaymentsApi
+from .polls_api import PollsApi
+from .realms_api import RealmsApi
+from .sites_api import SitesApi
+from .sphere_api import SphereApi
+from .stickers_api import StickersApi
+from .thoughts_api import ThoughtsApi
+from .tickets_api import TicketsApi
+from .wallet_api import WalletApi
+
+__all__ = [
+    'AccountsApi',
+    'AuthApi',
+    'ChatApi',
+    'DevelopersApi',
+    'DriveApi',
+    'E2EEApi',
+    'FitnessApi',
+    'NotificationsApi',
+    'PadlockApi',
+    'PaymentsApi',
+    'PollsApi',
+    'RealmsApi',
+    'SitesApi',
+    'SphereApi',
+    'StickersApi',
+    'ThoughtsApi',
+    'TicketsApi',
+    'WalletApi',
+]
